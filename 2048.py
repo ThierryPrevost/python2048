59b7a9323f23b25b6d101ecc019e06fc2c74bb14
widad oudad <widad.oudad@gmail.com>
2023-01-11 10:24:46 +0100

    init_grid2

--- 2048.py
+++ 2048.py
@@ -20,3 +20,26 @@ def init_grid():
     m[x2, y2] = 2
 
     return m
+
+
+def init_grid2():
+    grid = np.zeros((4, 4), dtype=int)
+
+    x1 = np.random.randint(4)
+    y1 = np.random.randint(4)
+
+    grid[x1, y1] = 2
+
+    x2 = x1
+    while x2 == x1:
+        x2 = np.random.randint(4)
+
+    y2 = y1
+    while y2 == y1:
+        y2 = np.random.randint(4)
+
+    grid[x2, y2] = 2
+    return grid
+
+grid1 = init_grid()
+print(grid1)
