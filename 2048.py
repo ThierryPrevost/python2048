4a913f94d7eee9cce0be1275c3a7e01aa7f69fb6
thierry prevost <thierryprevost333@gmail.com>
2023-01-11 10:13:58 +0100

    def init_grid

---
+++ 2048.py
@@ -0,0 +1,22 @@
+
+import numpy as np
+
+def init_grid():
+    m = np.zeros((4, 4), dtype=int)
+
+    x1 = np.random.randint(4)
+    y1 = np.random.randint(4)
+
+    m[x1, y1] = 2
+
+    x2 = x1
+    while x2 == x1:
+        x2 = np.random.randint(4)
+
+    y2 = y1
+    while y2 == y1:
+        y2 = np.random.randint(4)
+
+    m[x2, y2] = 2
+
+    return m
