bbf350182054f5e5aeec153b3f2672db0f415029
widad oudad <widad.oudad@gmail.com>
2023-01-11 10:47:04 +0100

    add_new2

--- 2048.py
+++ 2048.py
@@ -43,3 +43,31 @@ def init_grid2():
 
 grid1 = init_grid()
 print(grid1)
+
+def add_new2(grid):
+    taille = grid.shape
+    tailley = taille[0]
+    taillex = taille[1]
+
+    x2 = 0
+    y2 = 0
+    for y1 in range(tailley):
+
+        for x1 in range(taillex):
+
+            while grid[x1, y1] == 0 and y2 == 0:
+                y2 = np.random.randint(tailley)
+
+            while grid[x1, y1] == 0 and x2 == 0:
+                x2 = np.random.randint(taillex)
+
+    if np.random.randint(0, 100) < 80:
+        grid[x2, y2] = 2
+    else:
+        grid[x2, y2] = 4
+
+    return grid
+
+
+grid2 = add_new2(grid1)
+print(grid2)
