ca6c7dc002b61aacbbda9965aabbd7e2c31c31e3
thierry prevost <thierryprevost333@gmail.com>
2023-01-31 08:10:05 +0100

    stats

--- 2048.py
+++ 2048.py
@@ -1,5 +1,5 @@
-import numpy as np
 
+import numpy as np
 
 def init_grid():
     m = np.zeros((4, 4), dtype=int)
@@ -41,10 +41,8 @@ def init_grid2():
     grid[x2, y2] = 2
     return grid
 
-
 grid1 = init_grid2()
 #print(grid1)
-
 
 def add_new2(grid):
     taille = grid.shape
@@ -74,7 +72,6 @@ def add_new2(grid):
 grid2 = add_new2(grid1)
 #print(grid2)
 
-
 def add_new(grid):
     taille = grid.shape
     tailley = taille[0]
@@ -92,7 +89,7 @@ def add_new(grid):
             while grid[x1, y1] == 0 and x2 == 0:
                 x2 = np.random.randint(taillex)
 
-    if np.random.randint(0, 100) < 80:
+    if np.random.randint(0,100) < 80:
         grid[x2, y2] = 2
     else:
         grid[x2, y2] = 4
@@ -100,19 +97,19 @@ def add_new(grid):
     return grid
 
 
-row = [2, 4, 4, 2]  # >>>[2, 8, 2, 0]
 
-
+row = [2, 4, 4, 2]   # >>>[2, 8, 2, 0]
 def gauche(row):
-    return ([x for x in row if x != 0] + [0] * 4)[:4]
+
+    return ([x for x in row if x != 0]+[0]*4)[:4]
 
 #resultat = 0
 def rollin_row(row):
     #global resultat
     row = gauche(row)
     for i in range(1, 4):
-        if row[i] == row[i - 1]:
-            row[i - 1] *= 2
+        if row[i] == row[i-1]:
+            row[i-1] *= 2
             row[i] = 0
             #resultat += row[i-1]
     return gauche(row)
@@ -167,35 +164,3 @@ def rollin(grid, direction):
 gridnew = rollin(grid, 'd')
 print(gridnew)
 type(gridnew)
-
-
-
-
-print(rollin_row(row))
-
-
-grid3 = np.array([[8, 0, 16, 0], [4, 0, 0, 0], [4, 0, 2, 0], [0, 32, 2, 0]])
-print(grid3)
-
-v = [0] * 16
-def calcul(ligne, pas):
-    for i in range(4):
-        position = rollin_row([v[ligne[k]+pas*i] for k in range(4)])
-        for k in range(4):
-            v[ligne[k]+pas*i] = position[k]
-
-
-
-def rollin2(grid, direction):
-    # direction: l (left) r (right) u (up) and d (down)
-    new_grid = np.array(4, 4)
-
-    for i in range(1, 4):
-        for j in grid:
-            if direction == "l":
-                new_grid[i] = rollin_row(j)
-
-    return new_grid
-
-print(rollin(grid3, "l"))
-
