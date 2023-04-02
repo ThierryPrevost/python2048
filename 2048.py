1c99931a09e2f303ceee36357c60016828f1c183
thierry prevost <thierryprevost333@gmail.com>
2023-04-02 19:05:59 +0200

    modif add_new

--- 2048.py
+++ 2048.py
@@ -8,32 +8,7 @@ gridsize = 4
 screensize = 600
 gamedisp = pygame.display.set_mode((screensize, screensize))
 
-
 def init_grid():
-    m = np.zeros((4, 4), dtype=int)
-
-    x1 = np.random.randint(4)
-    y1 = np.random.randint(4)
-
-    m[x1, y1] = 2
-
-    x2 = x1
-    while x2 == x1:
-        x2 = np.random.randint(4)
-
-    y2 = y1
-    while y2 == y1:
-        y2 = np.random.randint(4)
-
-    m[x2, y2] = 2
-
-    return m
-
-
-# grid = init_grid()
-# print(grid)
-
-def init_grid2():
     grid = np.zeros((4, 4), dtype=int)
 
     x1 = np.random.randint(4)
@@ -50,61 +25,42 @@ def init_grid2():
         y2 = np.random.randint(4)
 
     grid[x2, y2] = 2
-    return grid
-
-
-# grid1 = init_grid2()
-# print(grid1)
-
-def add_new2(grid):
-    taille = grid.shape
-    tailley = taille[0]
-    taillex = taille[1]
-
-    x2 = 0
-    y2 = 0
-    for y1 in range(tailley):
-
-        for x1 in range(taillex):
-
-            while grid[x1, y1] == 0 and y2 == 0:
-                y2 = np.random.randint(tailley)
-
-            while grid[x1, y1] == 0 and x2 == 0:
-                x2 = np.random.randint(taillex)
-
-    if np.random.randint(0, 100) < 80:
-        grid[x2, y2] = 2
-    else:
-        grid[x2, y2] = 4
 
     return grid
 
 
-# grid2 = add_new2(grid1)
-# print(grid2)
+# grid = init_grid()
+# print(grid)
 
 def add_new(grid):
     taille = grid.shape
-    tailley = taille[0]
-    taillex = taille[1]
+    taillex = taille[0]
+    tailley = taille[1]
 
-    x2 = 0
-    y2 = 0
-    for y1 in range(tailley):
+    print('grid av:', grid)
 
-        for x1 in range(taillex):
+    x2 = -1
+    y2 = -1
 
-            while grid[x1, y1] == 0 and y2 == 0:
-                y2 = np.random.randint(tailley)
+    while x2 == -1 and y2 == -1:
 
-            while grid[x1, y1] == 0 and x2 == 0:
-                x2 = np.random.randint(taillex)
+        xtmp = np.random.randint(taillex)
+        ytmp = np.random.randint(tailley)
+
+        if grid[xtmp, ytmp] == 0:
+            x2 = xtmp
+            y2 = ytmp
 
     if np.random.randint(0, 100) < 80:
         grid[x2, y2] = 2
     else:
         grid[x2, y2] = 4
+
+
+    #print('x2:', x2)
+    #print('y2:', y2)
+    #print('grid apres:', grid)
+    #print('grid.shape:', grid.shape)
 
     return grid
 
